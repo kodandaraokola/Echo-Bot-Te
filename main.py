72e50084667ec24b679a9f218a1bf40e1b8dda12
VK4 <Kodandaramkola@gmail.com>
2023-07-14 16:52:30 -0400

    1111

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 import settings
 import discord
-
+from discord.ext import commands
 
 def run():
-    print(settings.DISCORD_API_SECRET)
-
+    intents = discord.Intents.default()
+    
+    bot = commands.Bot(command_prefix="!", intents=intents)
+    
+    @bot.event
+    async def on_ready():
+        print(bot.user)
+        print(bot.user.id)
+        print("____________")
+        
+    bot.run(settings.DISCORD_API_SECRET)
+    
 if __name__ == "__main__":
     run()
--- settings.py
+++ settings.py
@@ -4,4 +4,4 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DISCORD_API_SECRET = os.getenv("DISCORD_API_TOKEN")
+DISCORD_API_SECRET = os.getenv("DISCORD_API_TOKEN1")
