ff56e7fdb86650fe2cccfce24755dc651e45d9d9
VK4 <Kodandaramkola@gmail.com>
2023-07-14 16:38:50 -0400

    1

--- settings.py
+++ settings.py
@@ -4,4 +4,4 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DISCORD_API_SECRET = os.getenv("MTEyODcyNjk0NjE3MjM3OTI0OA.GD9SeO.UOPK13QqSwXAmA-ZYYDkjhBX-xCgCsJEltvLmw")
+DISCORD_API_SECRET = os.getenv("DISCORD_API_TOKEN")
