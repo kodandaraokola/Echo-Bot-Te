b9a4dc6d6a733a78185c36657d8ff98d2103f12a
VK4 <Kodandaramkola@gmail.com>
2023-07-14 16:34:54 -0400

    1

---
+++ main.py
@@ -0,0 +1,9 @@
+import settings
+import discord
+
+
+def run():
+    print(settings.DISCORD_API_SECRET)
+
+if __name__ == "__main__":
+    run()
--- settings.py
+++ settings.py
@@ -4,4 +4,4 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DISCORD_API_SECRET = os.get("MTEyODcyNjk0NjE3MjM3OTI0OA.GD9SeO.UOPK13QqSwXAmA-ZYYDkjhBX-xCgCsJEltvLmw")
+DISCORD_API_SECRET = os.getenv("MTEyODcyNjk0NjE3MjM3OTI0OA.GD9SeO.UOPK13QqSwXAmA-ZYYDkjhBX-xCgCsJEltvLmw")
